50a08d8edfa451584d65cc9c5073b68c283d91f6
Zhansaule Telisheva <jansaule97@mail.ru>
2024-04-14 21:49:29 +0500

    feat: add a solution of the second task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -36,8 +36,10 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    source.then(() => resolve('success')).catch(() => resolve('fail'));
+  });
 }
 
 /**
