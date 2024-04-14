509b96f774a52ca367ad6c7bc260b3980b47cf92
Zhansaule Telisheva <jansaule97@mail.ru>
2024-04-14 21:43:58 +0500

    feat: add a solution of the first task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,11 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number < 0) reject(new Error('Please, provide a positive number or 0'));
+    resolve(number);
+  });
 }
 
 /**
