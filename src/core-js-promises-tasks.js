9cdcf9894e4e5d6fff078bc653fd1ab0b419b80d
Zhansaule Telisheva <jansaule97@mail.ru>
2024-04-15 13:41:51 +0500

    feat: add a solutio  of the third task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -55,8 +55,15 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  let resPromise;
+  promises.forEach((promise) => {
+    promise.then(() => {
+      resPromise = promise;
+    });
+  });
+  if (resPromise === undefined) return promises[0];
+  return resPromise;
 }
 
 /**
