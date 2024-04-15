054bfd935ce6cc7cafa2f7f4ec98f2c3624b6b3e
Zhansaule Telisheva <jansaule97@mail.ru>
2024-04-15 13:55:46 +0500

    feat: add a solution of the fourth task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -83,8 +83,12 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(resolve).catch(reject);
+    });
+  });
 }
 
 /**
